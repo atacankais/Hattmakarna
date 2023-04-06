08d227aec93c2386aa92e28a1eef39bea0044625
ceciliaohberg <cissicecilia00@gmail.com>
2023-04-06 11:37:14 +0200

    :(

---
+++ src/hattmakarn/SeKunduppgifter.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hattmakarn;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+/**
+ *
+ * @author ceciliaohberg
+ */
+public class SeKunduppgifter {
+    
+    private InfDB idb;
+    
+   
+}
