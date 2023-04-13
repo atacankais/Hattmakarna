9f7130d1516db483d107495fe00a8c70a3855989
babanosh <71321428+babanosh@users.noreply.github.com>
2023-04-13 14:05:55 +0200

    Registrera kund

---
+++ src/hattmakarn/Validering.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hattmakarn;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author clarajonsson
+ */
+public class Validering {
+ //private static InfDB idb;
+ 
+ /*try {
+   idb = new InfDB ("hattdb", "3306", "hattdba", "hattkey");
+} catch (InfException ex) {
+    
+}
+    
+    
+private boolean kundTelefonFinnsRedan(JTextField telefonnummerTextruta){
+ 
+    boolean phoneExists = false;
+    
+    try {
+        String fraga = "Select Phone from Customer";
+        Arraylist <String> phoneList;
+        phoneList = idb.fetchColumn(fraga);
+        for
+    }
+    
+}*/
+    
+    
+    
+}
